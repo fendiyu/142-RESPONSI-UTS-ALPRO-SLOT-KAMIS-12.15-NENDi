14d438b3b5593bffb1495eca2e55c7198a6435c2
fendiyu <2400018142@webmail.uad.ac.id>
2025-05-31 15:13:46 +0700

    Add files via upload

---
+++ 142[responsi  uts alpro selasa 12.15-15.15.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int nama, password, inputbarang, menghapusbarang, totalbelanja, invoicepelanggan;
+	cout<<"masukkan nama: "<<endl;
+	cin>>nama;
+	cout<<"masukkan password: "<<endl;
+	cin>>password;
+	cout<<"admin menginput barang: "<<endl;
+    cin>>inputbarang;
+    cout<<"admin menghapus barang: "<<endl;
+    cin>>menghapusbarang;
+    cout<<"total belanja: "<<endl;
+    cin>>totalbelanja;
+    cout<<"invoice pelanggan: "<<endl;
+    cin>>invoicepelanggan;
+    
+    if(int i=1; i<=0; i++){
+	}else(int i=1; i<=0;){
+		for(int i=1; i>=0;)
+	}
+}
